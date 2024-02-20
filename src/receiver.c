686d8b2e2f5baf93fdff2e7b4c2febb5e49e86f5
cjohst <connor.johst@gmail.com>
2024-02-20 10:20:38 -0800

    skeleton for receiver.c more clearly defined

--- src/receiver.c
+++ src/receiver.c
@@ -13,17 +13,66 @@
 
 #include "packet.h"
 
+
 void rrecv(unsigned short int myUDPport, 
             char* destinationFile, 
             unsigned long long int writeRate) {
 
+    bool connection_open = false;
+    int sock_fd = initializeSocket(myUDPport);
 
-            server_sock = socket(
-                AF_INET,
-                SOCK_DGRAM,
-                0
-            )
+    connection_open = handshake(sock_fd);
 
+    while(connection_open){
+        """
+        CORE LOOP
+        read from socket into incoming buf
+        compute checksum
+        if checksum correct:
+            check flags:
+                if no flags:
+                    check seq#:
+                        if seq# correct:
+                            write data
+                            check if queued data can be written
+                            send ack
+                        else:
+                            hold data in queue
+                            await new data
+                if flags:
+                    start end connection protocol
+                    connection_open = false
+        else:
+            discard data
+        """
+    }            
+}
+
+bool handshake(int sock_fd){
+    //conduct tcp handshake
+}
+
+int initializeSocket(unsigned short int myUDPport){
+
+    int sock_fd;
+    struct sockaddr_in server_addr;
+    bool connection_open = false;
+
+    sock_fd = socket(
+        AF_INET,
+        SOCK_DGRAM,
+        IPPROTO_UDP
+    );
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(myUDPport);
+
+    //error checking for sock_fd
+    int bind_code = bind(sock_fd, (const struct sockaddr*) &server_addr, sizeof(server_addr));
+    //error checking for bind
+
+    return sock_fd;
 }
 
 int main(int argc, char** argv) {
