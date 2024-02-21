c7e256ee706caf1a18ca5469c522223eb47f129f
cjohst <connor.johst@gmail.com>
2024-02-21 11:32:33 -0800

    merging to main like a cowboy

--- src/receiver.c
+++ src/receiver.c
@@ -17,56 +17,69 @@
 
 #define MAXSIZE sizeof(struct packet)
 
-void rrecv(unsigned short int myUDPport, 
+void rrecv( unsigned short int myUDPport, 
             char* destinationFile, 
             unsigned long long int writeRate) {
 
     packet_t incoming_packet;
     packet_t outgoing_packet;
 
-    bool connection_open = false;
-    bool checksum = true;
+    int sock_fd;
+    struct sockaddr_in server_addr, client_addr;
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(myUDPport);
 
+    sock_fd = socket(
+        AF_INET,
+        SOCK_DGRAM,
+        IPPROTO_UDP
+    );
+
+    //error checking for sock_fd
+    int bind_code = bind(sock_fd, (const struct sockaddr*) &server_addr, sizeof(server_addr));
+    //error checking for bind
+
+
+    bool connection_open = true;
     uint32_t expected_sequence = 1;
-
-    int sock_fd = initializeSocket(myUDPport);
-    connection_open = handshake(sock_fd);
-
+   
     while(connection_open){
-        size_t recv_len = recvfrom(sock_fd, incoming_packet, sizeof(incoming_packet), MSG_WAITALL);
+        size_t recv_len = recvfrom(
+            sock_fd, 
+            incoming_packet, 
+            sizeof(incoming_packet), 
+            MSG_WAITALL, 
+            &client_addr, 
+            sizeof(client_addr)
+            );
         //some error handling on if recv_len is correct value?
         //compute checksum here, set value of checksum boolean
-        if (checksum){
-            if (HAS_FLAGS(incoming_packet->flags)){
-                //handle flags
-            }
-            else {
-                if(incoming_packet->sequence == expected_sequence){
-                    //write data
-                    //check if any enqueued data can be written
-
-                } else {
-                    //enqueue this packet somewhere
-                }
-                //create outgoing packet
-
-                //send ack
-
-            }
+        if (HAS_FLAGS(incoming_packet->flags)){
+            //handle flags
         }
         else {
-            //error handling for invalid checksum 
-            //likely discard
+            if(incoming_packet->sequence == expected_sequence){
+                //write data
+                //check if any enqueued data can be written
+
+            } else {
+                //enqueue this packet somewhere
+            }
+            //create outgoing packet
+
+            //send ack
+
         }
+    }
+    else {
+        //error handling for invalid checksum 
+        //likely discard
+    }
 
 
-    }            
-}
-
-
-bool handshake(int sock_fd){
-    //conduct tcp handshake
-}
+}            
 
 int initializeSocket(unsigned short int myUDPport){
 
