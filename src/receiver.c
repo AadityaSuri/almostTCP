9a143ce2048ec62c3eab5d7007d5caa82a815ce1
Aaditya Suri <aadityasuri1@gmail.com>
2024-03-01 21:48:24 -0800

    removed unused code from receiver.c

--- src/receiver.c
+++ src/receiver.c
@@ -215,24 +215,6 @@ void rrecv( unsigned short int udp_port,
         printf("%d START\n", packet_queue->size);
     }
 
-    
-
-
-    
-    //check if ANY enqueued data can be written and write it
-            // while(peak(packet_queue!=-1) && (packet_queue) <= expected_sequence) {
-            //     printf("PEAKING AT QUEUE\n");
-            //     if (peak(packet_queue) < expected_sequence){
-            //         //ensure we never write the same packet twice ()
-            //         QueueNode dequeued_node = dequeue(packet_queue);
-            //         continue;
-            //     }
-            //     QueueNode dequeued_node = dequeue(packet_queue);
-            //     printf("WRITING QUEUED  packet with seq_num: %d\n", dequeued_node.priority);
-            //     total_bytes_written += writeWithRate(dequeued_node.data, sizeof(dequeued_node.data), write_rate, total_bytes_written, start_time, outfile);
-            //     expected_sequence += 1;
-            // }
-
     printf("%d END\n", packet_queue->size);
 
     fclose(outfile);
